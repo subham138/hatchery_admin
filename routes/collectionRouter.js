69b771d4dd0b32045ececda4690412daa2f967b3
Subham <subham@synergicsoftek.in>
2024-02-27 22:39:09 +0530

    Change

--- routes/collectionRouter.js
+++ routes/collectionRouter.js
@@ -1,5 +1,9 @@
 const { getCompanyDtls } = require("../module/CompanyModule");
-const { allCollectionList, getBirdWeight, getLameBirdWeight } = require("../module/ReportModule");
+const {
+  allCollectionList,
+  getBirdWeight,
+  getLameBirdWeight,
+} = require("../module/ReportModule");
 
 const express = require("express"),
   collectionRouter = express.Router(),
@@ -14,50 +18,59 @@ collectionRouter.use((req, res, next) => {
   }
 });
 
-collectionRouter.all('/', async (req, res) => {
-    var method = req.method
-    var selected = {
-        frm_dt: method == 'POST' ? req.body.frm_dt : dateFormat(new Date, 'yyyy-MM-dd'),
-        to_dt: method == 'POST' ? req.body.to_dt : '',
-        active_flag: method == 'POST' ? true : false
-    }
-    var col_list = []
-    var comp_id = req.session.user.comp_id
+collectionRouter.all("/", async (req, res) => {
+  var method = req.method;
+  var selected = {
+    frm_dt:
+      method == "POST" ? req.body.frm_dt : dateFormat(new Date(), "yyyy-MM-dd"),
+    to_dt: method == "POST" ? req.body.to_dt : "",
+    active_flag: method == "POST" ? true : false,
+  };
+  var col_list = [];
+  var comp_id = req.session.user.comp_id;
 
-    if(method == 'POST'){
-        col_list = await allCollectionList(comp_id, selected.frm_dt, selected.to_dt)
-    }
+  if (method == "POST") {
+    col_list = await allCollectionList(
+      comp_id,
+      selected.frm_dt,
+      selected.to_dt
+    );
+  }
 
-    var view_dt = {
-      header: "Collection List",
-      col_list: method == "POST" ? (col_list.suc > 0 ? col_list.msg : []) : [],
-      selected,
-      dateFormat,
-    };
+  var view_dt = {
+    header: "Collection List",
+    col_list: method == "POST" ? (col_list.suc > 0 ? col_list.msg : []) : [],
+    selected,
+    dateFormat,
+  };
 
-    res.render("collection/view", view_dt);
-})
+  res.render("collection/view", view_dt);
+});
 
-collectionRouter.get('/edit', async (req, res) => {
-    var comp_id = req.session.user.comp_id
-    var dc_no = Buffer.from(decodeURIComponent(req.query.id), "base64").toString()
-    var col_list = await allCollectionList(0, 0, 0, dc_no),
+collectionRouter.get("/edit", async (req, res) => {
+  var comp_id = req.session.user.comp_id;
+  var dc_no = Buffer.from(
+    decodeURIComponent(req.query.id),
+    "base64"
+  ).toString();
+  var col_list = await allCollectionList(0, 0, 0, dc_no),
     birdWeight = await getBirdWeight(dc_no),
     lameBirdWeight = await getLameBirdWeight(dc_no),
     comp_dtls = await getCompanyDtls(comp_id);
 
-    var view_dt = {
-      header: "Edit Collection",
-      sub_header: "Collection List",
-      sub_header_url: "/collection",
-      data: col_list.suc > 0 ? col_list.msg : [],
-      birdWeight: birdWeight.suc > 0 ? birdWeight.msg : [],
-      lameBirdWeight: lameBirdWeight.suc > 0 ? lameBirdWeight.msg : [],
-      comp_dtls: comp_dtls.suc > 0 ? comp_dtls.msg : [],
-      dateFormat,
-    };
-    
-    res.render('collection/edit', view_dt)
-})
+  var view_dt = {
+    header: "Edit Collection",
+    sub_header: "Collection List",
+    sub_header_url: "/collection",
+    data: col_list.suc > 0 ? col_list.msg : [],
+    birdWeight: birdWeight.suc > 0 ? birdWeight.msg : [],
+    lameBirdWeight: lameBirdWeight.suc > 0 ? lameBirdWeight.msg : [],
+    comp_dtls: comp_dtls.suc > 0 ? comp_dtls.msg : [],
+    dateFormat,
+    id: dc_no,
+  };
+
+  res.render("collection/edit", view_dt);
+});
 
 module.exports = { collectionRouter };
