f4beeffe407d3f84d5823b9c550d75d078db5aac
Subham <subham@synergicsoftek.in>
2024-02-28 01:00:34 +0530

    Update 28.02.24

--- core/db.js
+++ core/db.js
@@ -1,12 +1,21 @@
 const mysql = require('mysql')
 
+// const db = mysql.createPool({
+//     connectionLimit: 10,
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "hatchery",
+// });
+
 const db = mysql.createPool({
-    connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "hatchery",
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "hatchery",
 });
+
 
 db.getConnection((err, connection) => {
     if(err) console.log(err);
